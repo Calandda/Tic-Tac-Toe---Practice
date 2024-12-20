a2597c755e2e4e19e835f88c0ea71b685b59427a
Calandda <A.calandda@gmail.com>
2024-12-20 18:53:46 +0800

    Added symbol assigning for Player 1 and Player 2

--- script.js
+++ script.js
@@ -1,21 +1,62 @@
 function createUser(name){
 	let score = 0;
+	let symbol = '';
+	let playerName = name;
+	function symbolAssign(inputSymbol){
+		symbol = inputSymbol;
+	};
 	function scoreAdd(){
 		score++;
-		console.log(score);
 	}
-	return scoreAdd;
+	function scoreReset(){
+		score = 0;
+	}
+	function getSymbol(){
+		return symbol;
+	}
+	function getName(){
+		return playerName;
+	}
+	return {scoreAdd, symbolAssign, scoreReset, getSymbol, getName};
 }
 function createGame(){
 	let place = [['','',''],['','',''],['','','']];
+	let roundCounter = 0;
+	let roundMax = 0;
+	function symbolAssign(player1,player2){
+		player1.symbolAssign('X');
+		player2.symbolAssign('O');
+	};
 	function scoreReset(){
 		place = [['','',''],['','',''],['','','']];
 	};
-	function scoreAdd(player,placement){
+	function roundCheck(){
+	}
+	function roundReset(){
+		roundCounter = 0;
+	};
+	function placeAdd(player,placement){
+		
 	};
 	function scoreCheck(){
+		return(place);
 	};
+	function scoreWin(){
+	}
+	function winCompare(player1,player2){
+	}
+	return {symbolAssign,
+	scoreReset,
+	roundReset,
+	placeAdd,
+	scoreCheck,
+	scoreWin,
+	winCompare};
 }
-const user1 = createUser('test');
-user1();
-user1();
+
+const tictactoe = createGame(); 
+const player1 = createUser('player1');
+const player2 = createUser('player2');
+tictactoe.symbolAssign(player1,player2);
+console.log('Player ' + player1.getName() + ' Assigned Symbol: ' + player1.getSymbol());
+console.log('Player ' + player2.getName() + ' Assigned Symbol: ' + player2.getSymbol());
