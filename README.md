1a9a0ba125ac0ca480ade0cd232f409336ec8ff3
Calandda <A.calandda@gmail.com>
2024-12-20 17:37:55 +0800

    Started Object creation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Tic-Tac-Toe---Practice
+
+	Tic Tac Toe made with HTML/Javascript/CSS; Objects and Closure practice
--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('test');
+function createUser(name){
+	let score = 0;
+	function scoreAdd(){
+		score++;
+		console.log(score);
+	}
+	return scoreAdd;
+}
+function createGame(){
+	let place = [['','',''],['','',''],['','','']];
+	function scoreReset(){
+		place = [['','',''],['','',''],['','','']];
+	};
+	function scoreAdd(player,placement){
+	};
+	function scoreCheck(){
+	};
+}
+const user1 = createUser('test');
+user1();
+user1();
